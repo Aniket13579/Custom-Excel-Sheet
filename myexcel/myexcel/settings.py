f359f9b35297792ff6da715474e9ad60a3fc1064
khushikansara <44203930+khushikansara@users.noreply.github.com>
2022-04-14 19:29:31 +0530

    Update settings.py

--- myexcel/myexcel/settings.py
+++ myexcel/myexcel/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jgb2f3(5=!75%9^9gd^psei#1&w6-hp-^kug^hj@(ecsx3lix%'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
